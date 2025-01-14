7ab6fa352b772f625b6bf336324b629b5b159a2e
Anish9667 <anishpal9667@gmail.com>
2025-01-14 12:13:39 +0530

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void Array(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    int arr[5] = {2, 4, 25, 64, 7};
+    int sizeofArray = 5;
+  //  Array(arr, 5);
+    cout<<sizeof(arr);
+}
